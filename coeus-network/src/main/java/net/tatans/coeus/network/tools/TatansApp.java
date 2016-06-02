3e7800a8f0c07bb58d44c6c70d37fbf15daf5d08
shengxiadeyu <Mac15985798735>
2016-06-02 21:11:41 +0800

    添加框架内的认证系统

--- coeus-network/src/main/java/net/tatans/coeus/network/tools/TatansApp.java
+++ coeus-network/src/main/java/net/tatans/coeus/network/tools/TatansApp.java
@@ -4,6 +4,10 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
 * @author 余亮 <br/>
 * 跟App相关的辅助类  
@@ -56,6 +60,27 @@ public class TatansApp
           e.printStackTrace();  
       }  
       return null;  
-  }  
+  }
+    /**
+     * [MD5加密工具]
+     *
+     * @return 当前应用的版本名称
+     */
+    public static String md5(String string) {
+        byte[] hash;
+        try {
+            hash = MessageDigest.getInstance("MD5").digest(string.getBytes("UTF-8"));
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Huh, MD5 should be supported?", e);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("Huh, UTF-8 should be supported?", e);
+        }
 
+        StringBuilder hex = new StringBuilder(hash.length * 2);
+        for (byte b : hash) {
+            if ((b & 0xFF) < 0x10) hex.append("0");
+            hex.append(Integer.toHexString(b & 0xFF));
+        }
+        return hex.toString();
+    }
 }
